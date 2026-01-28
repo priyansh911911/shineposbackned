8c3be4091a14531854fcfcb80088cd451bb17d56
Sayain-Aditya <as4316664@gmail.com>
2026-01-28 15:56:11 +0530

    sd

--- src/routes/zomatoWebhook.js
+++ src/routes/zomatoWebhook.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { handleZomatoWebhook } = require('../controllers/zomatoWebhookController');
 
-router.post('/webhook/zomato/items/:resId', handleZomatoWebhook);
 router.get('/webhook/zomato/:resId/items', (req, res) => {
   res.json({ message: 'Webhook endpoint ready', resId: req.params.resId });
 });
+router.post('/webhook/zomato/items/:resId', handleZomatoWebhook);
 
 module.exports = router;
